318dec05d8ee67d78bdcc5668806fe24d01ec1b0
Richard Spencer <richardspencer27@gmail.com>
2015-12-24 18:57:07 -0500

    Added Record label and equipped it with Autolayout after painful struggle

--- Pitch Perfect/Pitch Perfect/RecordSoundViewController.swift
+++ Pitch Perfect/Pitch Perfect/RecordSoundViewController.swift
@@ -14,6 +14,10 @@ class RecordSoundViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
